0546e00928cd5f93e3085073dc4d70216cea909b
Charmunks <im@char.best>
2025-12-31 17:06:53 +1100

    wording tweaks

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,15 @@
 import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-static';
+import rehypeSlug from 'rehype-slug';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: { adapter: adapter() },
-	preprocess: [mdsvex()],
+	preprocess: [
+		mdsvex({
+			rehypePlugins: [rehypeSlug]
+		})
+	],
 	extensions: ['.svelte', '.svx']
 };
 
